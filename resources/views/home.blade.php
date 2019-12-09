e81af92b368fa81fa86d970bbdb95c7ccfb4558f
Adama <adamadiagne662@gmail.com>
2019-12-09 10:55:15 +0000

    Essaie

---
+++ resources/views/home.blade.php
@@ -0,0 +1,214 @@
+@extends('layout')
+@section('contenue_page')
+       
+        <!-- Services -->
+        <section class="page-section" id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Services</h2>
+            <h3 class="section-subheading text-muted">Venez à la deouverte de GPmako.</h3>
+          </div>
+        </div>
+        <div class="row text-center">
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+              <i class="fas fa-piggy-bank fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">E-Conomique</h4>
+            <p class="text-muted">Vous faites toujours une bonne affaire sur GPmako, un service impeccable à moindre coups, un rapport qualite prix comme nulle part ailleurs.Alors ne vous le faites pas raconter!</p>
+          </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+              <i class="fas fa-people-carry fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">Sociabilité</h4>
+            <p class="text-muted">Derriere son aspect commerciale votre site GPmako est un lieu de rencontre sans meme vous en rendre compte vous élargissez votre carnet d'adresse sans meme quitter votre salon.</p>
+          </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fas fa-circle fa-stack-2x text-primary"></i>
+              <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+            </span>
+            <h4 class="service-heading">Sécurite</h4>
+            <p class="text-muted">Votre sécurite avant TOUT!<br> GPmako porte une attention particiliére à la sécurité de ses utilisateurs, aux respects de leurs droits et à leurs confidentialités</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="page-section" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Vous</h2>
+            <h3 class="section-subheading text-muted">Nous connaitre en peu plus.</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-12">
+            <ul class="timeline">
+              <li>
+                <div class="timeline-image">
+                  <img class="rounded-circle img-fluid" src="./image/5.jpg" alt="">
+                </div>
+                <div class="timeline-panel">
+                  <div class="timeline-heading">
+                    <h4>2009-2011</h4>
+                    <h4 class="subheading">Our Humble Beginnings</h4>
+                  </div>
+                  <div class="timeline-body">
+                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                  </div>
+                </div>
+              </li>
+              <li class="timeline-inverted">
+                <div class="timeline-image">
+                  <img class="rounded-circle img-fluid" src="./images/adama.jpg" alt="">
+                </div>
+                <div class="timeline-panel">
+                  <div class="timeline-heading">
+                    <h4>Sokhna Anta</h4>
+                    <h6 class="subheading">Super Client</h6>
+                  </div>
+                  <div class="timeline-body">
+                    <p class="text-muted">Sokhna est une cliente fidéle à GPmako, tenant une petite entreprise de cosmétique en Cote d'Ivoire elle sollicite la plus part du temps les services des transporteurs de GPmako pour ainsi assurer l'acheminement de ces commandes à bon port.</p>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div class="timeline-image">
+                  <img class="rounded-circle img-fluid" src="./images/ada.jpg" alt="">
+                </div>
+                <div class="timeline-panel">
+                  <div class="timeline-heading">
+                    <h4>A fait 3ans sur le site</h4>
+                    <h4 class="subheading"></h4>
+                  </div>
+                  <div class="timeline-body">
+                    <p class="text-muted">Adja Maguette est l'une de nos plus anciennes collaboratrices.Assurant chaque deux semaines un aller-retour Dakar-Singapour, Adja a toujours eu aue des retours positifs de ses clients rencontrés sur le site </p>
+                  </div>
+                </div>
+              </li>
+              <li class="timeline-inverted">
+                <div class="timeline-image">
+                  <img class="rounded-circle img-fluid" src="images/4.jpg" alt="">
+                </div>
+                <div class="timeline-panel">
+                  <div class="timeline-heading">
+                    <h4>July 2014</h4>
+                    <h4 class="subheading">Phase Two Expansion</h4>
+                  </div>
+                  <div class="timeline-body">
+                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                  </div>
+                </div>
+              </li>
+              <li class="timeline-inverted">
+                <div class="timeline-image">
+                  <h4>Be Part
+                    <br>Of Our
+                    <br>Story!</h4>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Clients -->
+  <section class="py-3 px-5">
+      
+        <div class="row">
+          
+          <div class="col-md-2 col-sm-4" style="float:left;">
+            <a href="#">
+              <img class="img-fluid d-block mx-auto" src="img/logos/nianga.png" alt="">
+            </a>
+          </div>
+          <div class="col-md-3 col-sm-6">
+            <a href="#">
+              <img class="img-fluid d-block mx-auto" src="img/logos/perfect.png" alt="">
+            </a>
+          </div>
+          
+          <div class="col-md-3 col-sm-4">
+            <a href="#">
+              <img class="img-fluid d-block mx-auto" src="img/logos/willomoda.png" alt="" height:"20px">
+            </a>
+          </div>
+          <div class="col-md-3 col-sm-4">
+            <a href="#">
+              <img class="img-fluid d-block mx-auto" src="img/logos/solution.png" alt="">
+            </a>
+          </div>
+          
+        </div>
+    
+    </section>
+  
+
+     <!-- Contact -->
+   <section class="page-section" id="contacts">
+     <div class="container">
+    <div class="containeur">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h3 class="section-heading text-uppercase">Recherche ton Transporteur</h3>
+          <h3 class="section-subheading text-muted">Dites nous en plus sur votre colis.</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-12">
+          <form id="contactForm" method="get" name="sentMessage" action="gpdispo.php">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="form-group">
+         <label for="paysdep">Entrez le pays de depart</label>
+                  <select class="form-control" name="paysdep" id="paysdep">
+                      <option value="1">Algerie</option>
+                      <option value="2">Allemagne</option>
+                      <option value="3">Angola</option>
+                      <option value="4">Algerie</option>
+                      <option value="5">Algerie</option>
+                      <option value="6">Algerie</option>
+                    </select>
+                  <p class="help-block text-danger"></p>
+                </div>
+                <div class="form-group">
+                  <input class="form-control" id="kg" name="kg" type="number" placeholder="Poids du colis " required="required" data-validation-required-message="Entrez un poids inferieur à 50kg.">
+                  <p class="help-block text-danger"></p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-group">
+                    <label for="paysarr" >Entrez le pays d'arrivée</label>
+                  <select class="form-control" name="paysarr" id="paysarr">
+                      <option value="1">Algerie</option>
+                      <option value="2">Allemagne</option>
+                      <option value="3">Angola</option>
+                      <option value="4">Algerie</option>
+                      <option value="5">Algerie</option>
+                      <option value="6">Algerie</option>
+                    </select>
+                  <p class="help-block text-danger"></p>
+                  <input class="form-control" id="date" name="date" type="text" placeholder="A partir" required="required" data-validation-required-message="Please enter your phone number.">
+                  <p class="help-block text-danger"></p>
+                 
+                </div>
+              </div>
+              <div class="clearfix"></div>
+              <div class="col-lg-12 text-center">
+                <div id="success"></div>
+                <button id="rechercher" class="btn btn-primary btn-xl text-uppercase" type="submit"><a href="envoyer.php"></a>Rechercher</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+@endsection
